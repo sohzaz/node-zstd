62c23639ac5e5e15d8884fcafe5b6e3f14258dd3
Drien Breton <drien.breton@gmail.com>
2016-09-14 20:11:29 +0900

    Added CompressionStream

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
-var cp =require('cp'),
+var cp =require('child_process'),
     Transform = require('stream').Transform;
 
 class CompressionStream extends Transform {
-
-
+    constructor(options) {
+        super(options);
+    }
+    _transform(chunk, encoding, callback) {
+        var proc = cp.spawn('zstd', [ '-c' , '-']);
+        var self = this;
+        proc.stdout.on('data', (data) => {
+            self.push(data);
+        });
+        proc.stdout.on('finish', () => {
+            callback();
+        });
+        proc.stderr.on('error', (err) => {
+            throw err;
+        });
+        proc.stdin.write(chunk);
+        proc.stdin.end();
+    }
 }
 
 class DecompressionStream extends Transform {
+    constructor(options) {
+        super(options);
+    }
+    _transform(chunk, encoding, callback) {
 
+    }
 }
 
 module.exports.CompressionStream = CompressionStream;
