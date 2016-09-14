a8099b53ce2425db045cd4b590541dfa067ca2a7
Drien Breton <drien.breton@gmail.com>
2016-09-14 22:09:50 +0900

    Added DecompressionStream

--- index.js
+++ index.js
@@ -15,19 +15,41 @@ class CompressionStream extends Transform {
             callback();
         });
         proc.stderr.on('error', (err) => {
-            throw err;
+            self.push(null);
+            callback(err);
         });
         proc.stdin.write(chunk);
         proc.stdin.end();
+    }
+
+    _flush() {
+        this.push(null);
     }
 }
 
 class DecompressionStream extends Transform {
     constructor(options) {
         super(options);
+        this.proc = cp.spawn('zstd', ['-c','-d', '-']);
+        var self = this;
+        this.proc.stdout.on('data', (data) => {
+            self.push(data);
+        });
+        this.proc.on('exit', () => {
+            self.push(null);
+        });
     }
     _transform(chunk, encoding, callback) {
+        this.proc.stderr.on('error', (err) => {
+            self.push(null);
+            callback(err);
+        });
+        this.proc.stdin.write(chunk);
+        callback();
+    }
 
+    _flush() {
+        this.proc.stdin.end();
     }
 }
 
